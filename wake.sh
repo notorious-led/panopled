66332bf9bcbcc321352789eaa551fcb2102afa36
Hunter Fuller <hfuller@pixilic.com>
2018-04-26 23:17:18 -0500

    Wake by simply rebooting
    This creates an immediate visual indication we've done it

--- wake.sh
+++ wake.sh
@@ -2,7 +2,8 @@
 
 for I in $(seq 0 60); do
     echo -n "$I "
-    snap rpc send multicast set_sleep_mode False
+    #snap rpc send multicast set_sleep_mode False
+    snap rpc send multicast reboot
     sleep 0.1
 done
 
