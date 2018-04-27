154a78d2220f147debeb12dfe7731dd03f6481c8
Hunter Fuller <hfuller@pixilic.com>
2018-04-26 23:20:00 -0500

    Wake 60 times then reboot 3 times

--- wake.sh
+++ wake.sh
@@ -2,8 +2,14 @@
 
 for I in $(seq 0 60); do
     echo -n "$I "
-    #snap rpc send multicast set_sleep_mode False
+    snap rpc send multicast set_sleep_mode False
+    sleep 0.1
+done
+
+for I in $(seq 0 2); do
+    echo -n "$I REBOOT "
     snap rpc send multicast reboot
     sleep 0.1
 done
+
 
