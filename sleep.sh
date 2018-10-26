8bfa0f74080e9d5d48432717ece615c2aa89d6c4
Hunter Fuller <hfuller@pixilic.com>
2018-10-25 23:42:45 -0500

    Actually fade out when we're about to sleep.

--- sleep.sh
+++ sleep.sh
@@ -2,7 +2,7 @@
 
 for I in $(seq 0 4); do
     echo -n "$I Setting effect "
-    snap rpc send multicast set_sleep_mode True
+    snap rpc send multicast set_current_effect 11
     sleep 0.1
 done
 for I in $(seq 0 4); do
